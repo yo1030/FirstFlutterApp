b5d26d935d08d24831de71c41aca306bdde91512
yo1030 <0vant1c@gmail.com>
2025-03-19 15:25:48 +0900

    プロジェクト初回作成

---
+++ first_app/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+void main() async {
+  // await dotenv.load();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => MyAppState(),
+      child: MaterialApp(
+        title: 'Namer App',
+        theme: ThemeData(
+          useMaterial3: true,
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        ),
+        home: MyHomePage(),
+      ),
+    );
+  }
+}
+
+class MyAppState extends ChangeNotifier {
+  var current = WordPair.random();
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('A random IDEA:!!'),
+          Text(appState.current.asLowerCase),
+        ],
+      ),
+    );
+  }
+}
